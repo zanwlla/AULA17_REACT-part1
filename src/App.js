6ff0b14137a820943cec5fd920f0be03533f85a8
Camila <zanellacamila016@gmail.com>
2024-10-18 10:52:06 -0300

    teste react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Nome from "./components/Nome"
+function App() {
+  return (
+    <div>
+      <h1>Nosso primeiro app em React</h1>
+
+      <h3>Usando componentes</h3>
+
+      <Nome pessoa="Camila"/>
+      <br/>
+      <Nome pessoa="Zanella"/>
+
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/Nome.js
@@ -0,0 +1,8 @@
+
+function Nome({pessoa}){
+    return(
+      <span>Bem vindo {pessoa} </span>
+    )
+  }
+
+export default Nome;
